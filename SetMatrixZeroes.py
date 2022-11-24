f688717496ae98c665e38a1106aede3a110d3a77
Kirtiraj Patil <31164882+kirito-kirtiraj@users.noreply.github.com>
2022-11-24 09:01:45 +0000

    HT medium set matrix zeroes

---
+++ SetMatrixZeroes.py
@@ -0,0 +1,30 @@
+class Solution:
+
+  def __init__(self):
+    self.zeroRows = {}
+    self.zeroCols = {}
+
+  def setZeroes(self, matrix: List[List[int]]) -> None:
+    """
+        Do not return anything, modify matrix in-place instead.
+        """
+    for rowIndex, row in enumerate(matrix):
+      for colIndex, i in enumerate(row):
+        if i == 0:
+          self.zeroRows[rowIndex] = True
+          self.zeroCols[colIndex] = True
+
+    # print(self.zeroRows)
+    # print(self.zeroCols)
+    for rowIndex, row in enumerate(matrix):
+      try:
+        if self.zeroRows[rowIndex]:
+          for i in range(0, len(row)):
+            matrix[rowIndex][i] = 0
+      except KeyError:
+        for colIndex, i in enumerate(row):
+          try:
+            if self.zeroCols[colIndex]:
+              matrix[rowIndex][colIndex] = 0
+          except KeyError:
+            continue
