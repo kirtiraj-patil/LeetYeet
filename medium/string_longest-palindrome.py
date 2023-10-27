6d1bebd932f19feb33d17dfd8b7e6003d6814fb3
Kirtiraj <officialkirtiraj@gmail.com>
2023-10-27 12:11:13 +0530

    string: longest palindrome

--- medium/string_longest-palindrome.py
+++ medium/string_longest-palindrome.py
@@ -1,32 +1,12 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        self.longestPalindrome = s[0]
-        self.s = s
-
-        if len(s) <= 1:
-            return s
-
+        ans=''
         for i in range(len(s)):
-            j = len(s) - 1
-            if len(self.longestPalindrome) > j - i + 1:
-                break
-            while j > i:
-                res = self.checkPalindrome(i, j)
-                if res:
-                    if len(self.longestPalindrome) < j - i + 1:
-                        self.longestPalindrome = s[i:j+1]
-                    break
-                else:
-                    j -= 1
-
-        return self.longestPalindrome
-
-                
-    def checkPalindrome(self, i , j):
-        if self.s[i] == self.s[j]:
-            if j - i <= 1:
-                return True
-            else:
-                return self.checkPalindrome(i + 1, j - 1)
-
-        return False
+            ans=max(ans,expand(s,i,i), expand(s,i,i+1), key=len)
+        return ans
+            
+def expand(s,i,j):
+    while i>=0 and j<len(s) and s[i]==s[j]:
+        i-=1
+        j+=1
+    return s[i+1:j]
