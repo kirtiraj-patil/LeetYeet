ac615bdf2eac7e2fcc2aa4336fef67c0f20464fb
Kirtiraj <officialkirtiraj@gmail.com>
2023-11-09 16:26:36 +0530

    array: move zeroes

---
+++ easy/array_move-zeroes.py
@@ -0,0 +1,14 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i = -1
+        j = 0
+        while j < len(nums):
+            if nums[j] != 0:
+                i += 1
+                nums[i], nums[j] = nums[j], nums[i]
+            j += 1
+
+        
