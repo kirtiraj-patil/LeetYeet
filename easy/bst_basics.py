57506cddb9a0f7e2e7a196ecdc9014c7aff8b85a
Kirtiraj <officialkirtiraj@gmail.com>
2023-11-10 19:42:54 +0530

    bst: deletion of a node

--- easy/bst_basics.py
+++ easy/bst_basics.py
@@ -4,8 +4,10 @@ class Node:
         self.left = None
         self.right = None
 
+
 class BST:
     root = None
+
     def __init__(self, nodes):
         self.root = Node(nodes[0])
         for node in nodes[1:]:
@@ -21,3 +23,82 @@ class BST:
                 root.right = self.insert(root.right, val)
 
         return root
+
+    def searchNode(self, val):
+        res = self.searchNodeHelper(self.root, val)
+        return res
+
+    def searchNodeHelper(self, root, val):
+        if root == None:
+            return None
+
+        if root.val == val:
+            return root
+        elif root.val > val:
+            return self.searchNodeHelper(root.left, val)
+        return self.searchNodeHelper(root.right, val)
+
+    def inorderSuccessor(self, root):
+        root = root.right
+        while root.left != None:
+            root = root.left
+        return root
+
+    def deleteNode(self, val, root=None):
+        parent = None
+        whichChild = None
+        currentNode = root or self.root
+
+        while currentNode != None:
+            if currentNode.val == val:
+                break
+            parent = currentNode
+            if currentNode.val < val:
+                currentNode = currentNode.right
+                whichChild = "right"
+            else:
+                currentNode = currentNode.left
+                whichChild = "left"
+
+        if currentNode == None:
+            return False
+
+        if parent == None or (currentNode.left != None and currentNode.right != None):
+            inSuccessor = self.inorderSuccessor(currentNode)
+            currentNode.val = inSuccessor.val
+            return self.deleteNode(inSuccessor.val, currentNode.right)
+        elif currentNode.left != None and currentNode.right == None:
+            if whichChild == "right":
+                parent.right = currentNode.left
+            else:
+                parent.left = currentNode.left
+        elif currentNode.left == None and currentNode.right != None:
+            if whichChild == "right":
+                parent.right = currentNode.right
+            else:
+                parent.left = currentNode.right
+        else:
+            if whichChild == "right":
+                parent.right = None
+            else:
+                parent.left = None
+
+        return True
+
+
+arr = [5, 2, 4, 1, 8, 11, 6, 9, 12, 10, 11.5]
+b = BST(arr)
+
+
+def printTree():
+    for n in arr:
+        r = b.searchNode(n)
+        if r == None:
+            continue
+        print(r.val, r.left and r.left.val, r.right and r.right.val)
+
+
+printTree()
+b.deleteNode(12)
+print("")
+printTree()
