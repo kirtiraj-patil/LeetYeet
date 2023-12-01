d363183374258f894af83f2fb64ab8d07f7038e9
Kirtiraj <officialkirtiraj@gmail.com>
2023-12-01 11:36:16 +0530

    array: max product subarray alt solution

--- medium/array_max-product-subarray.py
+++ medium/array_max-product-subarray.py
@@ -26,3 +26,31 @@ class Solution:
             ]
 
         return max(findMax(0, False))
+
+
+# Its all about having odd or even numbers of negative integers. if the negative numbers are even, then the first pass will give the solution. If the negative numbers are odd, the second pass will give the solution.
+
+# class Solution {
+#     public int maxProduct(int[] nums) {
+#         int prod = 1;
+#         int result = Integer.MIN_VALUE;
+
+#         for (int i = 0; i < nums.length; i++) {
+#             prod = prod * nums[i];
+#             result = Math.max(prod, result);
+#             if(prod == 0) {
+#                 prod = 1;
+#             }
+#         }
+#         prod = 1;
+
+#         for (int i = nums.length - 1; i >= 0; i--) {
+#             prod = prod * nums[i];
+#             result = Math.max(prod, result);
+#             if(prod == 0) {
+#                 prod = 1;
+#             }
+#         }
+#         return result;
+#     }
+# }
